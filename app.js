58817eef43123bc5f3f7d6c11281826e0597284d
Femi Obadimu <ficazzosam@gmail.com>
2024-10-16 13:50:30 +0100

    my second commit

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-this is my js file
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+    }
+);
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+    }
+);
+
